45c4c1c77d7275921793b6a04fb132885649fe80
anndonut <69615415+anndonut@users.noreply.github.com>
2020-09-10 14:09:27 +0900

    second commit

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,8 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    # ↓変更前：params.require(:calendars).permit(:date, :plan)
+    params.require(:calendars).permit(:content)
   end
 
   def getWeek
@@ -34,7 +35,13 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+       #Date.today.wdayを利用して添字となる数値を得る
+      wday_num = Date.today.wday[day] 
+      #もしもwday_numが7以上であれば、7を引く
+      if #条件式を記述
+        wday_num = wday_num - 7
+      end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans,wday: todays_date.wday}
       @week_days.push(days)
     end
 
